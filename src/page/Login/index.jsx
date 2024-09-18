1ad4dfc25c86f8fcd73e4033c35377b9f87da05f
E-jiwoo <livei07@naver.com>
2024-09-19 05:19:27 +0900

    add :: use LOTTERIACHAB font - 롯데리아챱 폰트 적용

--- src/page/Login/index.jsx
+++ src/page/Login/index.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import * as S from "./style";
 import oringE from "../../assets/oringE.svg";
 import logo from "../../assets/logo.svg";
-import welcome from "../../assets/welcome.svg";
 import mail from "../../assets/mail.svg";
 import password_img from "../../assets/password.svg";
 
@@ -22,35 +21,41 @@ const index = () => {
     <>
       <S.Container>
         <S.Background size={900} />
-        <S.Logo>
-          <S.LogoImg src={logo} alt="logo" />
-          <S.LogoText src={oringE} alt="oringE" />
-        </S.Logo>
-        <S.Welcome src={welcome} alt="welcome" />
-        <S.Text>로그인 후 더 다양한 기능을 누려보세요 :)</S.Text>
-        <S.Input>
-          <S.InputWrapper>
-            <S.Icon src={mail} alt="mail" />
-            <S.Mail
-              type="text"
-              placeholder="이메일"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </S.InputWrapper>
-          <S.InputWrapper>
-            <S.Icon src={password_img} alt="password" />
-            <S.Password
-              type="password"
-              placeholder="비밀번호"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </S.InputWrapper>
-        </S.Input>
-        <S.Login isFormFilled={isFormFilled} onClick={onClick}>
-          로그인
-        </S.Login>
+        <S.Contain>
+          <S.Logo>
+            <S.LogoImg src={logo} alt="logo" />
+            <S.LogoText src={oringE} alt="oringE" />
+          </S.Logo>
+          <S.Welcome>
+            오링에 온 걸<br />
+            환영해요!
+          </S.Welcome>
+          <S.Text>로그인 후 더 다양한 기능을 누려보세요 :)</S.Text>
+          <S.Input>
+            <S.InputWrapper>
+              <S.Icon src={mail} alt="mail" />
+              <S.Mail
+                type="text"
+                placeholder="이메일"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </S.InputWrapper>
+            <S.InputWrapper>
+              <S.Icon src={password_img} alt="password" />
+              <S.Password
+                type="password"
+                placeholder="비밀번호"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </S.InputWrapper>
+          </S.Input>
+          <S.Login isFormFilled={isFormFilled} onClick={onClick}>
+            로그인
+          </S.Login>
+          <S.Text>이메일 찾기 | 비밀번호 찾기 | 회원가입</S.Text>
+        </S.Contain>
       </S.Container>
     </>
   );
--- src/page/Login/style.js
+++ src/page/Login/style.js
@@ -5,48 +5,52 @@ export const Container = styled.div`
   height: 100vh;
   background-color: #f49e15;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+export const Contain = styled.div`
+  position: absolute;
+  top: 10rem;
 `;
 
 export const Background = styled.div`
   position: absolute;
   background-color: white;
-  width: 40rem;
-  height: 40rem;
+  width: 80rem;
+  height: 100rem;
   border-radius: ${(props) => props.size / 2}%;
-  top: 20%;
+  z-index: 0;
+  top: 15rem;
 `;
 
 export const Input = styled.div`
-  position: absolute;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  top: 19rem;
+  align-items: center;
+  margin-top: 2rem;
 `;
 
 export const InputWrapper = styled.div`
-  position: relative;
   display: flex;
   align-items: center;
 `;
 
 export const Mail = styled.input`
-  display: flex;
-  width: 10rem;
-  height: 2.4rem;
-  padding-left: 4rem;
-  padding-right: 2rem;
-  border: 1px #999999 solid;
+  width: 15rem;
+  height: 3rem;
+  padding-left: 3rem;
+  border: 1px solid #999999;
   border-radius: 8px 8px 0 0;
 `;
 
 export const Password = styled.input`
-  width: 10rem;
-  height: 2.4rem;
-  padding-left: 4rem;
-  padding-right: 2rem;
-  border: 1px #999999 solid;
+  width: 15rem;
+  height: 3rem;
+  padding-left: 3rem;
+  border: 1px solid #999999;
   border-top: none;
   border-radius: 0 0 8px 8px;
 `;
@@ -55,58 +59,54 @@ export const Icon = styled.img`
   position: absolute;
   width: 16px;
   height: 18px;
-  left: 2rem;
+  left: 1.3rem;
 `;
 
 export const Text = styled.div`
-  position: relative;
-  color: "white";
-  font-size: 35;
-  top: 14rem;
-  left: 2.2rem;
+  font-size: 1rem;
+  margin-top: 1rem;
+  text-align: center;
 `;
 
-export const Welcome = styled.img`
-  position: relative;
-  width: 183px;
-  height: 80px;
-  top: 7rem;
-  left: 16rem;
+export const Welcome = styled.div`
+  font-family: "LOTTERIACHAB";
+  font-size: 2.5rem;
+  -webkit-text-stroke: 1px black;
+  line-height: 2.8rem;
+  color: white;
+  margin-top: 1rem;
+  text-align: center;
 `;
 
 export const Logo = styled.div`
-  position: absolute;
-  top: 4rem;
-  left: 7rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const LogoImg = styled.img`
-  position: relative;
   width: 32px;
   height: 32px;
-  bottom: 0.2rem;
-  right: 0.2rem;
 `;
 
 export const LogoText = styled.img`
-  position: relative;
   width: 81px;
   height: 32px;
+  margin-left: 0.5rem;
 `;
 
 export const Login = styled.button`
-  position: relative;
-  width: 20rem;
-  height: 2rem;
-  top: 25rem;
-  right: 13.7rem;
-  color: white;
+  width: 18rem;
+  height: 3rem;
   background-color: ${(props) => (props.isFormFilled ? "#F49E15" : "#CCCCCC")};
   border: none;
   border-radius: 0.5rem;
+  color: white;
   font-size: 1rem;
   cursor: ${(props) => (props.isFormFilled ? "pointer" : "default")};
-  padding: 1rem 6.7rem 2rem 6.7rem;
+  margin-top: 2rem;
+  text-align: center;
   display: flex;
   justify-content: center;
+  align-items: center;
 `;
